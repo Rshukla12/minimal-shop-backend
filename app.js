c585d99623feea706fff013efee94a47e0bf9cdb
Ravi Shukla <rshuklabbb@gmail.com>
2021-07-18 21:24:08 +0530

    Added error handling

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require ('express');
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 const app = express();
 
+app.use(morgan('dev'));
 
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
