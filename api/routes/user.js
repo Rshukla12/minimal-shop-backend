67511244aeaf30872abe9beeffa8bbf2a43051a2
Ravi Shukla <rshuklabbb@gmail.com>
2021-07-26 23:47:33 +0530

    Added User Sign Up end point

---
+++ api/routes/user.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+router.post('/signup', (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then( user => {
+        if (user.length >= 1) {
+            return res.status(409).json({
+                message: 'email exists'
+            });
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user
+                    .save()
+                    .then( result => {
+                        res.status(201).json({
+                            message: 'user created'
+                        })
+                    })
+                    .catch( err => {
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+                }
+            });
+
+        }
+    })
+    .catch( err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+
+router.delete('/:userId', (req, res, next) => {
+    User.remove({_id: req.params.userId})
+    .exec()
+    .then ( result => {
+        res.status(200).json({
+            message: "user deleted"
+        })
+    })
+    .catch( err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-const Product = require('./api/models/product');
+const userRoutes = require('./api/routes/user');
 
 
 const connectDB = require('./config/db');
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
